6be6aaa0f1e2f5aa01e67e42f11a8ab396d87627
Graham Fletcher <marnburger@gmail.com>
2017-03-18 20:42:10 +0700

    Added kpi_4

--- kpis/index.js
+++ kpis/index.js
@@ -1,9 +1,11 @@
 const kpi_1 = require('./kpi_1');
 const kpi_2 = require('./kpi_2');
 const kpi_3 = require('./kpi_3');
+const kpi_4 = require('./kpi_4');
 
 module.exports = {
     kpi_1,
     kpi_2,
     kpi_3,
+    kpi_4,
 };
---
+++ kpis/kpi_4.js
@@ -0,0 +1,28 @@
+// Name of datasource this KPI uses
+// !!MUST MATCH THE FILENAME IN datasources FOLDER!!
+const datasource = 'aae';
+
+// Function to transform data
+const transformFunction = function (load) {
+    let transformedData = [];
+        load.data.forEach(loadDataItem => {
+            if (loadDataItem && //check item exists
+                loadDataItem.Name.length > 0 && //check it has field for Provider
+                loadDataItem["Attendances of Type 1 Departments - Major A&E"].toString().length > 0 && 
+                loadDataItem["Attendances of Type 1 Departments - Major A&E"].toString() !== 'N/A' && 
+                loadDataItem["Total attendances"].toString().length > 0 && 
+                loadDataItem["Total attendances"].toString() !== 'N/A'
+            ) {
+                transformedData.push({
+                    KPI_ID: 4,
+                    Period: load.Period,
+                    Provider: loadDataItem.Name,
+                    Value: loadDataItem["Attendances of Type 1 Departments - Major A&E"] / loadDataItem["Total attendances"],
+                    created_From: load._id
+                })
+            }
+        })
+    return transformedData; 
+}
+
+module.exports = {datasource, transformFunction};
