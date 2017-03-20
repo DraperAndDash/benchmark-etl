589ef26556309e66f1a973263e8ca0a70ae938e6
Graham Fletcher <marnburger@gmail.com>
2017-03-20 10:47:55 +0700

    Force load for April 2016

--- etl-helpers.js
+++ etl-helpers.js
@@ -112,7 +112,7 @@ function filterAndTransform(loadedData, id, transformFunction) {
 
 // TEMPORARY FUNCTION TO LOAD SPECIFIC FILES IN ETL-PROCESS SCRIPT 
 const transformData = function (datasource, id, transformFunction, mongo) {
-  return benchmarkAPI.findLoadByDatasourceFilename(datasource,"../nhs_england/September-2016-AE-by-provider-knmSR.xls").then(loadedData => {
+  return benchmarkAPI.findLoadByDatasourceFilename(datasource,"../nhs_england/April-2016-AE-by-provider-ZnB3z.xls").then(loadedData => {
     console.log('about to filter and transform loadedData for', id, 'in', datasource)
     return filterAndTransform(loadedData.data.loads, id, transformFunction)
   }).then(transformedData => {
