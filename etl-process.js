cfd6158076a5336a14b957e9b3476a2a7a6762a2
Graham Fletcher <marnburger@gmail.com>
2017-04-18 20:03:33 +0800

    Change concurrency

--- etl-process.js
+++ etl-process.js
@@ -7,7 +7,7 @@ const datasources = require('./datasources/');
 const kpis = require('./kpis/');
 const benchmarkAPI = require('./api/benchmark-api');
 
-const concurrency = 20;
+const concurrency = 5;
 const datasourceListGlobPattern = [
   './datasources/*.js',
   '!./datasources/index.js'
