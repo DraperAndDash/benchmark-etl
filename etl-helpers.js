2c339a1c3a0b005a5ae145a4bee4b9f00492552c
Graham Fletcher <marnburger@gmail.com>
2017-04-18 20:13:36 +0800

    Changed etl-helpers

--- etl-helpers.js
+++ etl-helpers.js
@@ -130,7 +130,7 @@ const transformData = function (datasource, id, transformFunction, mongo) {
 const transformDataByFile = function (file, datasource, id, transformFunction, mongo) {
   return benchmarkAPI.findLoadByDatasourceFilename(datasource, file).then(loadedData => {
     console.log('about to filter and transform loadedData for', id, 'in', datasource)
-    console.log('find me',loadedData)
+    // console.log('find me',loadedData)
     return filterAndTransform(loadedData.data.loads, id, transformFunction)
   }).then(transformedData => {
     console.log('about to save transformed data for', id, 'in', datasource)
