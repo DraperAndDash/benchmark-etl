85911704b13eb322ad2d6b7cdafd6c027667df4a
Graham Fletcher <marnburger@gmail.com>
2017-04-25 17:55:45 +0800

    Push for ca3 prod load

--- etl-process.js
+++ etl-process.js
@@ -10,7 +10,7 @@ const benchmarkAPI = require('./api/benchmark-api');
 const concurrency = 10;
 const datasourceListGlobPattern = [
   // './datasources/*.js',
-  './datasources/ca2*.js',
+  './datasources/ca3*.js',
   '!./datasources/index.js'
 ];
 const kpiListGlobPattern = [
