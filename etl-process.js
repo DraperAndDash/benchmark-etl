3f094bf24a262fb890f0e5d4585669678601bc8d
Graham Fletcher <marnburger@gmail.com>
2017-05-06 22:14:45 +0800

    Push for run with just kpi 34

--- etl-process.js
+++ etl-process.js
@@ -10,7 +10,7 @@ const benchmarkAPI = require('./api/benchmark-api');
 const concurrency = 5;
 const datasourceListGlobPattern = [
   // './datasources/*.js',
-  './datasources/rtt*.js',
+  // './datasources/rtt*.js',
   '!./datasources/dementia.js',
   '!./datasources/index.js'
 ];
