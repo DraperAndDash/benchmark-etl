2fc846e7be88802deab567628f7339c8373118a4
Graham Fletcher <marnburger@gmail.com>
2017-05-06 22:48:38 +0800

    Push for rtti prod load

--- etl-process.js
+++ etl-process.js
@@ -10,7 +10,7 @@ const benchmarkAPI = require('./api/benchmark-api');
 const concurrency = 5;
 const datasourceListGlobPattern = [
   // './datasources/*.js',
-  './datasources/rttna.js',
+  './datasources/rtti.js',
   '!./datasources/dementia.js',
   '!./datasources/index.js'
 ];
