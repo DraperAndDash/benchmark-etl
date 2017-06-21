68758c8fbb51534610d1cc08ce6711ff0b164fb4
Graham Fletcher <marnburger@gmail.com>
2017-06-21 09:32:37 -0500

    Edited update-kpis for using updateOne mongoose function in index

--- update-kpis.js
+++ update-kpis.js
@@ -9,19 +9,19 @@ const kpiInfoFile = "../Benchmarking_Lookup.xlsx";
 mongoXlsx.xlsx2MongoData(kpiInfoFile, {}, function(err, mongoData) {
     mongoData[1].forEach(kpi => {
         console.log(kpi)
-        benchmarkAPI.getKPIByID(kpi.KPI_ID).then(foundKpi => {
-            if (foundKpi.data.length === 0) {
+        // benchmarkAPI.getKPIByID(kpi.KPI_ID).then(foundKpi => {
+        //     if (foundKpi.data.length === 0) {
                 let newKPI = {
                     KPI_ID: kpi.KPI_ID,
                     KPI_name: kpi.KPI_name,
                     KPI_description: kpi.KPI_description,
                     KPI_area: kpi.KPI_area,
-                    datasource: kpi.datasource,
+                    datasource: kpi.datasource, 
                     frequency: kpi.frequency,
                     format: kpi.format
                 }
                 benchmarkAPI.postKPI(newKPI)
-            } 
-        })
+            // } 
+        // })
     })
 })
